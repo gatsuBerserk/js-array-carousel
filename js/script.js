30e75e89149f81cb194e25e2e45d40e48cb76fb1
gatsuBerserk <berserkdev@outlook.it>
2022-03-01 16:04:11 +0100

    divide the problem into steps. Insert first img

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Array con immagini
+const images = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg',
+]; 
+
+console.log(images)
+
+// Abbiamo lateralmente a sinistra 5 Img. (Proviamo con una)
+    // 1) Inseriamo l'immagine nel riquadro sinistro
+
+
+
+
+// Quando ne selezioniamo 1 deve apparire nel riquadro centrale. (Proviamo con la prima)
