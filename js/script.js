0bf9ec9b40a0e0e3b2dcd50eefb28c77177253d2
gatsuBerserk <berserkdev@outlook.it>
2022-03-07 14:40:19 +0100

    to finish and comment

--- js/script.js
+++ js/script.js
@@ -1,38 +1,95 @@
 // Array con immagini
 const images = [
     'img/01.jpg',
-    // 'img/02.jpg',
-    // 'img/03.jpg',
-    // 'img/04.jpg',
-    // 'img/05.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg',
 ]; 
 
-console.log(images)
-
-carouselLeft ="";
-// Abbiamo lateralmente a sinistra 5 Img. (Proviamo con una)
-    // 1) Inseriamo l'immagine nel riquadro sinistro 
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise', 
     
-    for (let i = 0 ; i < images.length ; i++){ 
-        carouselLeft += `
-        <div class="my-size-left border-bottom">
-        <img src="${images[i]}" class="my-img" alt="Random picture">
-        </div>`
+];
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,', 
+    
+] 
+
+
+
+let imgCentral= document.querySelector("div.my-img"); 
+let coverImg="";
+let leftImg= document.querySelector("div.dom-multi-img");
+let imgPrew=""; 
+
+for(let i = 0; i < images.length; i++ ){
+    coverImg += 
+    ` 
+
+        <img class="my-size-img-central" src="${images[i]}" alt="">         
+        
+    `
+    imgPrew +=
+    ` 
+    <div class="my-size-left">
+        <img class="my-img-left img-fluid" src="${images[i]}" alt="">
+    </div> 
+    `
+
+};
+imgCentral.innerHTML= coverImg; 
+leftImg.innerHTML= imgPrew; 
+
+// elementi visibili uno per volta
+
+let activeElement= 0;
+document.getElementsByClassName("my-size-img-central")[activeElement].classList.add("active"); 
+document.getElementsByClassName("my-img-left")[activeElement].classList.add("active");
+
+
+// arrow
+const up = document.querySelector("div.arrow-up");
+
+up.addEventListener("click", function() {
+    
+    document.getElementsByClassName("my-size-img-central")[activeElement].classList.remove("active"); 
+    
+    if(activeElement === 0){
+        activeElement= images.length -1; 
+    }else{
+        activeElement--;
     }; 
-    const carouselWrapper = document.querySelector('div.multi-img');
-    carouselWrapper.innerHTML += carouselLeft;
-// § fine aggiunta -->
+
+    
+    document.getElementsByClassName("my-size-img-central")[activeElement].classList.add("active");
+    
+    
+}); 
+
+const down= document.querySelector("div.arrow-down");
+down.addEventListener("click", function() {
+    document.getElementsByClassName("my-size-img-central")[activeElement].classList.remove("active");
+   
+
+    if( activeElement === images.length -1){
+        activeElement= 0;
+    }else{
+        activeElement++;
+    }
+    
+    document.getElementsByClassName("my-size-img-central")[activeElement].classList.add("active");
+    
+});  
 
 
-// Quando ne selezioniamo 1 deve apparire nel riquadro centrale. (Proviamo con la prima)
-let carouselCentralContent = '';
 
-for (let i = 0 ; i < images.length ; i++){
-   carouselCentralContent += 
-	`<div class="my-img">
-        <img class="my-size-img-central" src="img/01.jpg" alt="">
-    </div>`
-}; 
- 
-const centralWrapper= document.querySelector("div.central-img"); 
-centralWrapper.innerHTML+=carouselCentralContent;
